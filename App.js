dd59ec644284a48d692c98eba7dd219a55a91607
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-10-23 10:52:15 +0530

    Set up redux-thunk middleware

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 export default class App extends React.Component {
   render() {
