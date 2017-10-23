e8afa15171543907b3061b26624affa4df7c9219
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-10-23 14:42:43 +0530

    Set up individual deck view

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { View, Text, StatusBar as DefaultStatusBar } from 'react-native'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { StackNavigator } from 'react-navigation'
 import { Constants } from 'expo'
 import Decks from './components/decks'
+import DeckView from './components/deck-view'
 import reducer from './reducers'
-import { black } from './utils/colors'
+import { black, white } from './utils/colors'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
@@ -28,13 +30,31 @@ function StatusBar () {
   )
 }
 
+const MainNavigation = StackNavigator({
+  Home: {
+    screen: Decks,
+    navigationOptions: {
+      header: null
+    }
+  },
+  DeckView: {
+    screen: DeckView,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: black
+      }
+    }
+  }
+})
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <View style={{ flex: 1 }}>
           <StatusBar />
-          <Decks />
+          <MainNavigation />
         </View>
       </Provider>
     )
---
+++ components/deck-view.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { white } from '../utils/colors'
+
+class DeckView extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { title } = navigation.state.params
+    return { title }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Deck view</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default DeckView
