a3af3206f2876a8b0a0e558cb9ea4d5aa894b5c9
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-10-23 12:20:31 +0530

    Display loading icon on wait

--- components/decks.js
+++ components/decks.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
 import { connect } from 'react-redux'
 import { getDecks } from '../actions'
 
@@ -8,19 +8,38 @@ class Decks extends Component {
     this.props.getDecks()
   }
 
+  isEmpty = store => Object.keys(store).length === 0
+
   render() {
+    const store = this.props.store
     return (
-      <Text style={styles.text}>
-        {JSON.stringify(this.props.store, null, 2)}
-      </Text>
+      <View style={styles.container}>
+        {
+          store === null
+            ? <Text>
+                You haven't added any decks yet
+              </Text>
+            : this.isEmpty(store)
+                ? <ActivityIndicator
+                    style={{ marginTop: 30 }}
+                    size={30}
+                  />
+                : <Text>
+                    {JSON.stringify(store, null, 2)}
+                  </Text>
+        }
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    textAlign: 'center',
-    marginTop: 20
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20
   }
 })
 
