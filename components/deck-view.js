0cc3b8adf178d3dc273fc9ad605517491e0bdaae
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-10-23 15:15:31 +0530

    Display deck data from store

--- components/deck-view.js
+++ components/deck-view.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native'
+import { connect } from 'react-redux'
 import { white } from '../utils/colors'
 
 class DeckView extends Component {
@@ -9,9 +10,34 @@ class DeckView extends Component {
   }
 
   render() {
+    const { store, navigation } = this.props
+    const { title } = navigation.state.params
+    const deck = store[title]
+    const cardsCount = deck.questions && deck.questions.length
     return (
       <View style={styles.container}>
-        <Text>Deck view</Text>
+        <Text>{title}</Text>
+        <Text>
+          {`${cardsCount} card${cardsCount === 1 ? '' : 's'}`}
+        </Text>
+        <TouchableNativeFeedback
+          useForeground
+          background={TouchableNativeFeedback.Ripple('#CCCCCC', true)}
+          style={{ flex: 1 }}
+        >
+          <View>
+            <Text>Add card</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback
+          useForeground
+          background={TouchableNativeFeedback.Ripple('#606060', true)}
+          style={{ flex: 1 }}
+        >
+          <View>
+            <Text>Start Quiz</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     )
   }
@@ -26,4 +52,6 @@ const styles = StyleSheet.create({
   }
 })
 
-export default DeckView
+const mapStateToProps = store => ({ store })
+
+export default connect(mapStateToProps)(DeckView)
