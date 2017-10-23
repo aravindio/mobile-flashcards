10ec4225b57e1d0c5219b7292722b322ec8eb0cc
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-10-23 11:04:35 +0530

    Set up Status Bar

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StatusBar as DefaultStatusBar } from 'react-native'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { Constants } from 'expo'
 import reducer from './reducers'
+import { black } from './utils/colors'
 
 const store = createStore(reducer, applyMiddleware(thunk))
+
+function StatusBar () {
+  const props = {
+    style: {
+      backgroundColor: black,
+      height: Constants.statusBarHeight
+    }
+  }
+  return (
+    <View { ...props }>
+      <DefaultStatusBar
+        translucent
+        backgroundColor={black}
+        barStyle='light-content'
+      />
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Hello world</Text>
+        <View style={{ flex: 1 }}>
+          <StatusBar />
+          <Text style={{ textAlign: 'center' }}>Hello world</Text>
         </View>
       </Provider>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
