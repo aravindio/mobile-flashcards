4bccf424cffba51368216f1c50bb9982700ed000
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-10-23 12:51:51 +0530

    Display & style FlatList

--- components/decks.js
+++ components/decks.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  TouchableOpacity
+} from 'react-native'
 import { connect } from 'react-redux'
 import { getDecks } from '../actions'
+import { gray, darkGray } from '../utils/colors'
 
 class Decks extends Component {
   componentWillMount() {
     this.props.getDecks()
   }
 
-  isEmpty = store => Object.keys(store).length === 0
+  isEmpty = store => Object.keys(Object.assign({}, store)).length === 0
+
+  changeShape = store =>
+    [
+      ...Object.keys(Object.assign({}, store)).map(title => ({
+        title: store[title].title,
+        cardsCount: store[title].questions.length
+      }))
+    ]
+
+  renderItem = ({ item }) => {
+    const { title, cardsCount } = item
+    return (
+      <View style={styles.touchableContainer}>
+        <TouchableOpacity>
+          <View style={styles.cardContainer}>
+            <Text style={styles.cardTitle}>{title}</Text>
+            <Text style={styles.cardsCount}>
+              {`${cardsCount} card${cardsCount === 1 ? '' : 's'}`}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 
   render() {
-    const store = this.props.store
+    const { store } = this.props
+    const data = this.changeShape(store)
+    const props = {
+      style: data.length > 0 ? {} : styles.container
+    }
     return (
-      <View style={styles.container}>
+      <View { ...props }>
         {
           store === null
             ? <Text>
@@ -24,9 +60,11 @@ class Decks extends Component {
                     style={{ marginTop: 30 }}
                     size={30}
                   />
-                : <Text>
-                    {JSON.stringify(store, null, 2)}
-                  </Text>
+                : <FlatList
+                    data={data}
+                    keyExtractor={(item, index) => index}
+                    renderItem={this.renderItem}
+                  />
         }
       </View>
     )
@@ -38,8 +76,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    alignSelf: 'center'
+  },
+  touchableContainer: {
+    borderBottomColor: gray,
+    borderBottomWidth: 1,
+    borderStyle: 'solid'
+  },
+  cardContainer: {
+    flex: 1,
     paddingLeft: 20,
-    paddingRight: 20
+    paddingRight: 20,
+    paddingTop: 40,
+    paddingBottom: 40
+  },
+  cardTitle: {
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  cardsCount: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: darkGray
   }
 })
 
--- utils/colors.js
+++ utils/colors.js
@@ -1,3 +1,4 @@
 export const black = '#202020'
 export const white = '#FFFFFF'
 export const gray = '#EEEEEE'
+export const darkGray = '#BBBBBB'
