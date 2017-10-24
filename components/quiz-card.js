b7ae4be32eaa1b5066ea79737f9805755dbda532
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-10-24 09:26:35 +0530

    Create card flip animation

--- components/quiz-card.js
+++ components/quiz-card.js
@@ -5,33 +5,97 @@ import {
   Text,
   StyleSheet,
   Dimensions,
-  TouchableOpacity
+  TouchableOpacity,
+  Animated
 } from 'react-native'
 import Button from './button'
 import { white, gray, black } from '../utils/colors'
 
 export default class QuizCard extends Component {
+  state = {
+    animated: new Animated.Value(0),
+    front: '',
+    back: '',
+    flipped: false
+  }
+
+  componentWillMount() {
+    const { animated } = this.state
+    const front = animated.interpolate(
+      { inputRange: [0, 180], outputRange: ['0deg', '180deg'] }
+    )
+    const back = animated.interpolate(
+      { inputRange: [0, 180], outputRange: ['180deg', '360deg'] }
+    )
+    this.setState({ front, back })
+  }
+
+  componentDidUpdate() {
+    const { animated, flipped } = this.state
+    const transformOptions = { toValue: !flipped ? 0 : 180 }
+    Animated.spring(animated, transformOptions).start()
+  }
+
+  optionClick = option => {
+    const { correct, incorrect } = this.props
+    option ? correct() : incorrect()
+    if (this.state.flipped)
+      this.setState({ flipped: false })
+  }
+
   render() {
-    const { currentQuestion, questions, correct, incorrect } = this.props
+    const { currentQuestion, questions } = this.props
+    const { front, back, flipped } = this.state
+    const zIndex = flipped ? 0 : 2
+    const frontStyle = { transform: [{ rotateY: front }], zIndex }
+    const backStyle = { transform: [{ rotateY: back }] }
+    let questionScrollViewProps = {}
+    let answerScrollViewProps = {}
+    if (questions[currentQuestion].question.length < 100)
+      questionScrollViewProps.contentContainerStyle = styles.scrollView
+    if (questions[currentQuestion].answer.length < 100)
+      answerScrollViewProps.contentContainerStyle = styles.scrollView
     return (
       <View style={styles.container}>
         <Text style={styles.quizStatus}>
           {`Question ${currentQuestion + 1} of ${questions.length}`}
         </Text>
-        <View style={styles.cardContainer}>
-          <ScrollView contentContainerStyle={styles.scrollView}>
-            <View style={styles.cardTextContainer}>
-              <Text style={{ fontSize: 30 }}>
-                {questions[currentQuestion].question}
-              </Text>
-            </View>
-          </ScrollView>
-          <TouchableOpacity style={styles.cardButton}>
-            <Text style={{ color: white }}>Answer</Text>
-          </TouchableOpacity>
+        <View>
+          <Animated.View style={[styles.cardContainer, frontStyle]}>
+            <ScrollView { ...questionScrollViewProps }>
+              <View style={styles.cardTextContainer}>
+                <Text style={styles.cardText}>
+                  {questions[currentQuestion].question}
+                </Text>
+              </View>
+            </ScrollView>
+            <TouchableOpacity
+              onPress={() => this.setState({ flipped: true })}
+              style={styles.cardButton}
+            >
+              <Text style={{ color: white }}>Answer</Text>
+            </TouchableOpacity>
+          </Animated.View>
+          <Animated.View
+            style={[backStyle, styles.cardContainer, styles.back]}
+          >
+            <ScrollView { ...answerScrollViewProps }>
+              <View style={styles.cardTextContainer}>
+                <Text style={styles.cardText}>
+                  {questions[currentQuestion].answer}
+                </Text>
+              </View>
+            </ScrollView>
+            <TouchableOpacity
+              onPress={() => this.setState({ flipped: false })}
+              style={styles.cardButton}
+            >
+              <Text style={{ color: white }}>Question</Text>
+            </TouchableOpacity>
+          </Animated.View>
         </View>
-        <Button onPress={correct}>Correct</Button>
-        <Button onPress={incorrect}>Incorrect</Button>
+        <Button onPress={() => this.optionClick(true)}>Correct</Button>
+        <Button onPress={() => this.optionClick()}>Incorrect</Button>
       </View>
     )
   }
@@ -41,7 +105,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignSelf: 'stretch',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center'
   },
   quizStatus: {
@@ -60,7 +124,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: gray,
     borderRadius: 2,
-    marginBottom: 30
+    marginBottom: 30,
+    backgroundColor: white
+  },
+  back: {
+    position: 'absolute',
+    top: 0
   },
   scrollView: {
     flex: 1,
@@ -69,6 +138,10 @@ const styles = StyleSheet.create({
   cardTextContainer: {
     padding: 20,
     paddingBottom: 50
+  },
+  cardText: {
+    fontSize: 30,
+    textAlign: 'center'
   },
   cardButton: {
     paddingTop: 5,
